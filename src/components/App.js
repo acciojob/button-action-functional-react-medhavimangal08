c69b488bdaa364a60613c3a26d3b367fb52737a9
medhavimangal08 <medhavimangal43@gmail.com>
2024-12-24 00:23:32 +0530

    ustate

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para, setPara] = useState("");
+  function buttonclickedfunc() {
+    setPara(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      {/* // Do not alter the main div */}
+      <p id="para">{para}</p>
+      <button id="click" onClick={buttonclickedfunc}>
+        Show me the sentence
+      </button>
     </div>
   );
 }
-
 
 export default App;
